271f38e0baadee294be5356bb38d753059263bba
zura <68401942+zura3395@users.noreply.github.com>
2021-11-02 14:29:15 -0700

    Update troll.cpp

--- troll/src/troll.cpp
+++ troll/src/troll.cpp
@@ -11,14 +11,53 @@
 #include <iomanip>
 using namespace std;
 
+/*
+
+int func(int salesWest, int salesEast) {
+	salesWest++;
+	salesWest = salesWest + salesEast;
+	return salesWest;
+}
+
 int main() {
-    cout << "Time start!" << endl;
-    long start = 0;
-    long end = 0;
-    start = clock();
-    for (int i; i<1000000000; )
-        i++;
-    end = clock();
-    cout << setprecision(8) << fixed << difftime(end, start)/CLOCKS_PER_SEC << " seconds."<< endl;
+    int var1, sum, salesWest;
+    var1 =3;
+    salesWest =4;
+    sum = func(salesWest, var1);
+    cout << "salesWest = " << sum;
     return 0;
+}
+
+
+int func6 (int a, int &b) {
+	a++;
+	b++;
+	return a+b;
+}
+
+int main () {
+	int var1 = 10;
+	int var2 = 20;
+	cout << var1 << " " << var2 << endl;
+	int sum = func6 (var1, var2);
+	cout << var1 << " " << var2 << endl;
+	return 0;
+}
+
+*/
+
+double funcA (double parameter) {
+	if (parameter < 100) {
+		return 15;
+	}
+	else if (parameter >= 100) {
+		return 16;
+	}
+}
+
+int main () {
+	int i = 100;
+	double cock = funcA(i);
+	cout << cock;
+	return 0;
 }
